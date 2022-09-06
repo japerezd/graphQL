6deb16919bd13cc389a86b8bafacf95b6b2edfdc
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-09-06 13:23:57 -0500

    Add query variables

--- job-board/client/src/components/JobDetail.js
+++ job-board/client/src/components/JobDetail.js
@@ -1,11 +1,20 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import { jobs } from '../fake-data';
+import { getJob } from '../graphql/queries';
 
 function JobDetail() {
+  const [job, setJob] = useState(null);
   const { jobId } = useParams();
 
-  const job = jobs.find((job) => job.id === jobId);
+  useEffect(() => {
+    getJob(jobId).then(setJob);
+  }, [jobId])
+
+  if (!job) {
+    return <p>Loading...</p>
+  }
+  
   return (
     <div>
       <h1 className="title">
