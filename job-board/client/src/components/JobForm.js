b394a37f1c11c84e427194ff137d6127202d8a14
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-09-07 16:48:59 -0500

    Send mutation request

--- job-board/client/src/components/JobForm.js
+++ job-board/client/src/components/JobForm.js
@@ -1,12 +1,18 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router'
+import { createJob } from '../graphql/queries';
 
 function JobForm() {
+  const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     console.log('should post a new job:', { title, description });
+    const companyId = 'wvdB54Gqbdp_NZTXK9Tue' // FIXME
+    const job = await createJob({title, companyId, description});
+    navigate(`/jobs/${job.id}`)
   };
 
   return (
--- job-board/client/src/graphql/queries.js
+++ job-board/client/src/graphql/queries.js
@@ -2,7 +2,21 @@ import { request, gql } from 'graphql-request';
 
 const GRAPHQL_URL = 'http://localhost:9000/graphql';
 
-export async function createJob(id) {
+export async function createJob(input) {
+  const query = gql`
+    mutation CreateJobMutation($input: CreateJobInput!) {
+      job: createJob(input: $input) {
+        id
+      }
+    }
+  `;
+
+  const variables = { input };
+  const { job } = await request(GRAPHQL_URL, query, variables);
+  return job;
+}
+
+export async function getCompany(input) {
   const query = gql`
     query CompanyQuery($id: ID!) {
       company(id: $id) {
@@ -17,28 +31,9 @@ export async function createJob(id) {
     }
   `;
 
-  const variables = { id };
+  const variables = { input };
   const { company } = await request(GRAPHQL_URL, query, variables);
   return company;
-}
-
-export async function getCompany(input) {
-  const query = gql`
-    mutation CreateJobMutation($input: CreateJobInput!) {
-      job: createJob(input: $input) {
-        id
-        title
-        company {
-          id
-          name
-        }
-      }
-    }
-  `;
-
-  const variables = { input };
-  const { job } = await request(GRAPHQL_URL, query, variables);
-  return job;
 }
 
 export async function getJob(id) {
