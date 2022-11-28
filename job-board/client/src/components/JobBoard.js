aeb0db54e121f76add88a1836db397feff283762
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-11-28 12:27:00 -0600

    Add custom hooks

--- job-board/client/src/components/JobBoard.js
+++ job-board/client/src/components/JobBoard.js
@@ -1,11 +1,8 @@
 import JobList from './JobList';
-import { JOBS_QUERY } from '../graphql/queries'
-import { useQuery } from '@apollo/client';
+import { useJobs } from '../graphql/hooks';
 
 function JobBoard() {
-  const { data, loading, error } = useQuery(JOBS_QUERY, {
-    fetchPolicy: 'network-only',
-  });
+  const { jobs, loading, error } = useJobs();
 
   if (loading) {
     return <p>Loading...</p>
@@ -14,7 +11,6 @@ function JobBoard() {
     return <p>Sorry, something went wrong</p>
   }
 
-  const { jobs } = data;
   return (
     <div>
       <h1 className="title">
---
+++ job-board/client/src/graphql/hooks.js
@@ -0,0 +1,14 @@
+import { useQuery } from "@apollo/client";
+import { JOBS_QUERY } from "./queries";
+
+export function useJobs() {
+  const { data, loading, error } = useQuery(JOBS_QUERY, {
+    fetchPolicy: 'network-only',
+  });
+
+  return {
+    jobs: data?.jobs,
+    loading,
+    error: Boolean(error),
+  }
+}
