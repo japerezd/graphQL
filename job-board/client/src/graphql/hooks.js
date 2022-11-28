acbf663190e0d3f507941f92486c9835168d4936
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-11-28 13:04:27 -0600

    Create useCreateJob mutation

--- job-board/client/src/graphql/hooks.js
+++ job-board/client/src/graphql/hooks.js
@@ -1,5 +1,33 @@
-import { useQuery } from "@apollo/client";
-import { COMPANY_QUERY, JOBS_QUERY, JOB_QUERY } from "./queries";
+import { useMutation, useQuery } from "@apollo/client";
+import { getAccessToken } from "../auth";
+import { COMPANY_QUERY, CREATE_JOB_MUTATION, JOBS_QUERY, JOB_QUERY } from "./queries";
+
+export function useCreateJob() {
+  const [mutate, { loading, error }] = useMutation(CREATE_JOB_MUTATION);
+  return {
+    createJob: async (title, description) => {
+      const { data: { job } } = await mutate({ 
+        variables: { input: { title, description }},
+        context: {
+          headers: { Authorization: `Bearer ${getAccessToken()}` }
+        },
+        update: (cache, { data: { job } }) => {
+          cache.writeQuery({
+            query: JOB_QUERY,
+            variables: { id: job.id },
+            data: {
+              // data we want to write (always an object)
+              job,
+            },
+          });
+        },
+      });
+      return job;
+    },
+    loading,
+    error: Boolean(error),
+  }
+}
 
 export function useJob(id) {
   const { data, loading, error } = useQuery(JOB_QUERY, {
